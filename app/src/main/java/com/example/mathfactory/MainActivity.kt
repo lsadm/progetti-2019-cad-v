921a94a99f886573b619ab4d59b081f5fe4c1909
freer <raffaele.maddaloni@studenti.unicampania.it>
2019-05-11 13:43:35 +0200

    Nuova versione della Main_Activity

--- app/src/main/java/com/example/mathfactory/MainActivity.kt
+++ app/src/main/java/com/example/mathfactory/MainActivity.kt
@@ -13,79 +13,13 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.math.*
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
-
+    var controllo=true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         nav_view.setNavigationItemSelectedListener(this)
+        button21.setOnClickListener {if(controllo){ textView31.text="-Sroll from left to right\nto access to the menu\n-Version 1.0\n-Created by:\nRaffaele Maddaloni\nand\nGiuseppe Barbato";button21.text="Hide";button21.setTextColor(rgb(155,17,30));controllo=false}else{textView31.text="";button21.text="Details";button21.setTextColor(rgb(40,114,51));controllo=true}}
     }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_application, menu)
-        return true
-    }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.getItemId()
-        if (id == R.id.action_one) {
-            Toast.makeText(this, "MathFact version 1.0", Toast.LENGTH_LONG).show()
-            return true
-        }
-        if (id == R.id.action_two) {
-            Toast.makeText(this, "Enjoy with Math!", Toast.LENGTH_LONG).show()
-            return true
-        }
-        if (id == R.id.action_three) {
-            Toast.makeText(this, "Created by: Raffaele Maddaloni ®", Toast.LENGTH_LONG).show()
-            return true
-        }
-        if (id == R.id.action_four) {
-            val next = Intent(this, Function1::class.java)
-            startActivity(next)
-            return true
-        }
-        if (id == R.id.action_five) {
-            val next = Intent(this, Function2::class.java)
-            startActivity(next)
-            return true
-        }
-        if (id == R.id.action_six) {
-            val next = Intent(this, Function3::class.java)
-            startActivity(next)
-            return true
-        }
-        if (id == R.id.action_seven) {
-            val next = Intent(this, Function4::class.java)
-            startActivity(next)
-            return true
-        }
-        if (id == R.id.action_eight) {
-            val next = Intent(this, Function5::class.java)
-            startActivity(next)
-            return true
-        }
-        if (id == R.id.action_nine) {
-            val next = Intent(this, Function6::class.java)
-            startActivity(next)
-            return true
-        }
-        if (id == R.id.action_ten) {
-            val next = Intent(this, Function7::class.java)
-            startActivity(next)
-            return true
-        }
-        if (id == R.id.action_history_window) {
-            val next = Intent(this, Function8::class.java)
-            startActivity(next)
-            return true
-        }
-        if (id == R.id.calculator) {
-            val next = Intent(this, Function0::class.java)
-            startActivity(next)
-            return true
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
     override fun onNavigationItemSelected(item: MenuItem): Boolean
     {
         val id = item.getItemId()
