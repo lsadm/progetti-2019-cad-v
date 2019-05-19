f9547da10887414ea9f607c11cace5259cbcce46
freer <raffaele.maddaloni@studenti.unicampania.it>
2019-05-19 16:17:07 +0200

    Inizio Implementazione Function1

--- app/src/main/java/com/example/mathfactory/Function1.kt
+++ app/src/main/java/com/example/mathfactory/Function1.kt
@@ -1,23 +1,33 @@
 package com.example.mathfactory
 
 import android.content.Intent
+import android.graphics.Color.rgb
 import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.NavigationView
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_function1.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class Function1 : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private var mediaplayer: MediaPlayer?=null
+    private var switch= arrayOf(true,true,true,true,true,true)
+    var identifier:Int=0
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_function1)
         nav_view1.setNavigationItemSelectedListener(this)
+        button8.setOnClickListener {if((switch[0]&&switch[1]&&switch[2]&&switch[3]&&switch[4]&&switch[5])||!switch[0]){identifier=1;if(switch[identifier-1]){setta_elementi(switch[identifier-1],identifier);switch[identifier-1]=false;set_color(identifier)}else{setta_elementi(switch[identifier-1],identifier);switch[identifier-1]=true;set_color(0)}}}
+        button9.setOnClickListener {if((switch[0]&&switch[1]&&switch[2]&&switch[3]&&switch[4]&&switch[5])||!switch[1]){ identifier=2;if(switch[identifier-1]){setta_elementi(switch[identifier-1],identifier);switch[identifier-1]=false;set_color(identifier)}else{setta_elementi(switch[identifier-1],identifier);switch[identifier-1]=true;set_color(0)}}}
+        button10.setOnClickListener {if((switch[0]&&switch[1]&&switch[2]&&switch[3]&&switch[4]&&switch[5])||!switch[2]){identifier=3;if(switch[identifier-1]){setta_elementi(switch[identifier-1],identifier);switch[identifier-1]=false;set_color(identifier)}else{setta_elementi(switch[identifier-1],identifier);switch[identifier-1]=true;set_color(0)}}}
+        button30.setOnClickListener {if((switch[0]&&switch[1]&&switch[2]&&switch[3]&&switch[4]&&switch[5])||!switch[3]){identifier=4;if(switch[identifier-1]){setta_elementi(switch[identifier-1],identifier);switch[identifier-1]=false;set_color(identifier)}else{setta_elementi(switch[identifier-1],identifier);switch[identifier-1]=true;set_color(0)}}}
+        button31.setOnClickListener {if((switch[0]&&switch[1]&&switch[2]&&switch[3]&&switch[4]&&switch[5])||!switch[4]){identifier=5;if(switch[identifier-1]){setta_elementi(switch[identifier-1],identifier);switch[identifier-1]=false;set_color(identifier)}else{setta_elementi(switch[identifier-1],identifier);switch[identifier-1]=true;set_color(0)}}}
+        button11.setOnClickListener {if((switch[0]&&switch[1]&&switch[2]&&switch[3]&&switch[4]&&switch[5])||!switch[5]){identifier=6;if(switch[identifier-1]){setta_elementi(switch[identifier-1],identifier);switch[identifier-1]=false;set_color(identifier)}else{setta_elementi(switch[identifier-1],identifier);switch[identifier-1]=true;set_color(0)}}}
     }
     override fun onCreateOptionsMenu(menu: Menu):Boolean
     {
@@ -120,4 +130,128 @@ class Function1 : AppCompatActivity(), NavigationView.OnNavigationItemSelectedLi
         }
         return true
     }
+    private fun setta_elementi(controllo:Boolean,tipo:Int)
+    {
+        if(controllo)
+        {
+            button20.visibility= View.VISIBLE
+            button17.visibility= View.VISIBLE
+            textView30.visibility= View.VISIBLE
+            when(tipo)
+            {
+                1->
+                {
+                    textView24.visibility=View.VISIBLE
+                    textView26.visibility=View.VISIBLE
+                    editText4.visibility=View.VISIBLE
+                    editText6.visibility=View.VISIBLE
+                    textView29.visibility=View.VISIBLE
+                    editText10.visibility=View.VISIBLE
+                    textView24.text="First Side Length"
+                    textView26.text="Second Side Lenght"
+                    textView29.text="Swept Corner Width"
+                }
+                2->
+                {
+                    textView24.visibility=View.VISIBLE
+                    textView26.visibility=View.VISIBLE
+                    editText4.visibility=View.VISIBLE
+                    editText6.visibility=View.VISIBLE
+                    textView24.text="Angular Coefficient"
+                    textView26.text="Origin\nIntercept"
+                }
+                3->
+                {
+                    textView24.visibility=View.VISIBLE
+                    textView26.visibility=View.VISIBLE
+                    editText4.visibility=View.VISIBLE
+                    editText6.visibility=View.VISIBLE
+                    textView29.visibility=View.VISIBLE
+                    editText10.visibility=View.VISIBLE
+                    textView24.text="Origin\nAbscissa"
+                    textView26.text="Origin\nOrdinate"
+                    textView29.text="Radius"
+                }
+                4->
+                {
+                    textView24.visibility=View.VISIBLE
+                    textView26.visibility=View.VISIBLE
+                    editText4.visibility=View.VISIBLE
+                    editText6.visibility=View.VISIBLE
+                    textView29.visibility=View.VISIBLE
+                    editText10.visibility=View.VISIBLE
+                    textView24.text="a"
+                    textView26.text="b"
+                    textView29.text="c"
+                }
+                5->
+                {
+                    textView24.visibility=View.VISIBLE
+                    textView26.visibility=View.VISIBLE
+                    editText4.visibility=View.VISIBLE
+                    editText6.visibility=View.VISIBLE
+                    textView29.visibility=View.VISIBLE
+                    textView28.visibility=View.VISIBLE
+                    editText10.visibility=View.VISIBLE
+                    editText9.visibility=View.VISIBLE
+                    textView24.text="Origin\nAbscissa"
+                    textView26.text="Origin\nOrdinate"
+                    textView29.text="Semi-Minor Axis"
+                    textView28.text="Semi-Major Axis"
+                }
+                6->
+                {
+                    textView24.visibility=View.VISIBLE
+                    textView26.visibility=View.VISIBLE
+                    editText4.visibility=View.VISIBLE
+                    editText6.visibility=View.VISIBLE
+                    textView29.visibility=View.VISIBLE
+                    textView28.visibility=View.VISIBLE
+                    editText10.visibility=View.VISIBLE
+                    editText9.visibility=View.VISIBLE
+                    textView24.text="a"
+                    textView26.text="b"
+                    textView29.text="c"
+                    textView28.text="d"
+                }
+            }
+        }
+        else
+        {
+            button20.visibility= View.INVISIBLE
+            button17.visibility= View.INVISIBLE
+            textView30.visibility= View.INVISIBLE
+            textView24.visibility=View.INVISIBLE
+            textView26.visibility=View.INVISIBLE
+            editText4.visibility=View.INVISIBLE
+            editText6.visibility=View.INVISIBLE
+            textView29.visibility=View.INVISIBLE
+            textView28.visibility=View.INVISIBLE
+            editText10.visibility=View.INVISIBLE
+            editText9.visibility=View.INVISIBLE
+        }
+    }
+    private fun set_color(id:Int)
+    {
+        val not_selected=rgb(81,153,167)
+        val selected=rgb(255,215,0)
+        val selected2=rgb(40,114,51)
+        val selected3=(rgb(155,17,30))
+        button8.setTextColor(not_selected)
+        button9.setTextColor(not_selected)
+        button10.setTextColor(not_selected)
+        button30.setTextColor(not_selected)
+        button31.setTextColor(not_selected)
+        button11.setTextColor(not_selected)
+        when(id)
+        {
+            1->button8.setTextColor(selected)
+            2->button9.setTextColor(selected)
+            3->button10.setTextColor(selected)
+            4->button30.setTextColor(selected)
+            5->button31.setTextColor(selected)
+            6->button11.setTextColor(selected)
+        }
+    }
 }
+
