a17fe4d78ccda3891ad3cdebc19fe0f3c5d8e157
freer <raffaele.maddaloni@studenti.unicampania.it>
2019-06-15 07:39:37 +0200

    Correzione orologio e perfezionamento ticchettio dell'orologio

--- app/src/main/java/com/example/mathfactory/Fragment_Clock.kt
+++ app/src/main/java/com/example/mathfactory/Fragment_Clock.kt
@@ -2,6 +2,7 @@ package com.example.mathfactory.com.example.mathfactory
 import android.content.Context
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Environment
 import android.os.Handler
 import android.os.Message
 import android.support.v4.app.Fragment
@@ -12,6 +13,7 @@ import com.example.mathfactory.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_clock.*
 import kotlinx.android.synthetic.main.fragment_clock.view.*
+import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
 var regolatore_clock:Boolean?=null
@@ -50,41 +52,60 @@ class Fragment_Clock:Fragment()
                     progressTimer2.progress = formato_minuti.format(Date()).toInt()
                     progressTimer3.progress = formato_secondi.format(Date()).toInt()
                 }
-                else
-                    if(valore=="setTime")
-                    {
-                        ora = formato1.format(Date()).toString()
-                        data = formato2.format(Date()).toString()
-                        view.timerView.text = ora
-                        view.dateView.text = data
-                    }
+                ora = formato1.format(Date()).toString()
+                data = formato2.format(Date()).toString()
+                view.timerView.text = ora
+                view.dateView.text = data
+
             }
         }
         class ClockThread constructor(val handler: Handler):Thread()
         {
-            var mediaplayer:MediaPlayer?=null
+            var mediaplayer_tic:MediaPlayer?=null
+            var mediaplayer_tac:MediaPlayer?=null
             override fun run()
             {
-                mediaplayer= MediaPlayer.create(contesto_Fragment_Clock,R.raw.ticchettio_sound)
+                var alternatore:Boolean
+                val file= File(Environment.getExternalStorageDirectory().absolutePath+"/.MathView/"+utente_loggato+"/MathView_Parameters/Parameter5.txt")
+                if(file.exists())
+                    alternatore=!file.readText(Charsets.UTF_8).toBoolean()
+                else
+                    alternatore = true
+                mediaplayer_tic= MediaPlayer.create(contesto_Fragment_Clock,R.raw.tic_sound)
+                mediaplayer_tac= MediaPlayer.create(contesto_Fragment_Clock,R.raw.tac_sound)
                 notify_message("synchronize")
+                if(alternatore)
+                    mediaplayer_tic?.start()
+                else
+                    mediaplayer_tac?.start()
                 while(regolatore_clock==true)
                 {
-                    notify_message("setTime")
                     sleep(1000)
                     if(regolatore_clock==true)
                     {
                         progressTimer3.incrementProgressBy(1)
-                        if (progressTimer3.progress == progressTimer3.max)
+                        if (progressTimer3.progress == 60)
                         {
                             progressTimer2.incrementProgressBy(1)
                             progressTimer3.progress = 0
                         }
-                        if (progressTimer2.progress == progressTimer2.max)
+                        if (progressTimer2.progress == 60)
                         {
                             progressTimer.incrementProgressBy(1)
                             progressTimer2.progress = 0
                         }
-                        mediaplayer?.start()
+                        if (progressTimer.progress == 24)
+                        {
+                            progressTimer3.incrementProgressBy(1)
+                            progressTimer.progress = 0
+                        }
+                        notify_message("setTime")
+                        alternatore=!alternatore
+                        file.writeText(alternatore.toString(),Charsets.UTF_8)
+                        if(alternatore)
+                            mediaplayer_tic?.start()
+                        else
+                            mediaplayer_tac?.start()
                     }
                 }
             }
